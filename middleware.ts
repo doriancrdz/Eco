189d65622bf7f3f894dbeede60d76be236a714bd
cdorian654-8902 <cdorian654@yahoo.com>
2026-02-12 18:50:35 +0100

    Revert settings protection to client redirect

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware(async (auth, req) => {
-  const pathname = req.nextUrl.pathname;
-
-  // Protect /settings
-  if (pathname.startsWith("/settings")) {
-    await auth.protect();
-  }
-
-  // (Optionnel) Protect APIs too:
-  // if (pathname.startsWith("/api/")) await auth.protect();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!_next|.*\\..*).*)"],
