e1048690d66ae45de2573f37be322efeb17aa50d
cdorian654-8902 <cdorian654@yahoo.com>
2026-02-13 18:16:59 +0100

    Fix Clerk SignIn page as client component

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center aura-gradient">
-      <SignIn />
+      <SignIn routing="path" path="/sign-in" />
     </div>
   );
 }
